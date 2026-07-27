b9bd6b2d3a5fe5b79a720aa36dbc2ce3f5b421e8
baqdaulet <sakenuli1604@gmail.com>
2026-07-27 14:02:25 +0500

    Fix Railway preview host

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-});
+preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+}
